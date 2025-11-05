9d9ef0c0f1bb383dd40388ff692b6894eaa6f3d7
Ceren Kartal <kartalceren01@gmail.com>
2025-11-05 22:28:01 +0000

    Configure static files for deployment

--- machinetranslationsite/settings.py
+++ machinetranslationsite/settings.py
@@ -63,6 +63,7 @@ EMAIL_HOST_PASSWORD = 'xuzq kunx wmtu layc'
 # own code ends
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -141,6 +142,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
