5e98bc5c7d5b9e5fd14b128d851cc7fce9485afc
Ceren Kartal <kartalceren01@gmail.com>
2025-11-05 22:20:12 +0000

    Remove hardcoded SECRET_KEY

--- machinetranslationsite/settings.py
+++ machinetranslationsite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-i-vwkz5f6a%6bs0lmfl6%1zd_!)g@^d0a-&i)t2khfk8%*bie2')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-localfallback')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
